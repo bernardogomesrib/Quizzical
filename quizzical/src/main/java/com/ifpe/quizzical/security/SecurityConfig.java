b4c11d8a15aabb8e848aaf5a78ee530d11f3335a
Bernardo Gomes <bernardogomesrib@gmail.com>
2023-11-21 18:21:23 -0300

    a

--- quizzical/src/main/java/com/ifpe/quizzical/security/SecurityConfig.java
+++ quizzical/src/main/java/com/ifpe/quizzical/security/SecurityConfig.java
@@ -1,5 +1,59 @@
 package com.ifpe.quizzical.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+    @Order(1)
+
+    @Bean
+    public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+            authorizeRequests
+                .requestMatchers(new AntPathRequestMatcher("/"))
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+            ).oauth2Login(oauth2 -> oauth2
+            .defaultSuccessUrl("/"))
+        .logout(logout -> logout
+            .logoutSuccessUrl("/"));
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+            authorizeRequests
+                .requestMatchers(new AntPathRequestMatcher("/*"))
+                .hasRole("USER")
+                .anyRequest()
+                .authenticated()
+        ).oauth2ResourceServer(oauth2 ->
+            oauth2.jwt(Customizer.withDefaults())
+        );
+        return http.build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
